cfce83b5ac99609caca4c33b1e4550524840e4b3
mike <opensource@hoho.com>
2010-01-18 18:58:18 -0800

    Add symbol list.

---
+++ lcdui/common.py
@@ -0,0 +1,25 @@
+from lcdui import util
+
+SYMBOL = util.Enum(*(
+  'ARROW_UP',
+  'ARROW_DOWN',
+  'ARROW_LEFT',
+  'ARROW_RIGHT',
+
+  'TRIANGLE_UP',
+  'TRIANGLE_DOWN',
+  'TRIANGLE_LEFT',
+  'TRIANGLE_RIGHT',
+
+  'PROGBAR_1',
+  'PROGBAR_2',
+  'PROGBAR_3',
+  'PROGBAR_4',
+  'PROGBAR_5',
+
+  'MENU_CURSOR',
+  'MENU_LIST_UP',
+  'MENU_LIST_DOWN',
+
+  'FRAME_BACK',
+))
