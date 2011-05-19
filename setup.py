223d971d872b520880205ccfc65ebf20d8bb6cff
mike <opensource@hoho.com>
2011-05-18 21:10:48 -0700

    Bump to v0.5.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "pylcdui",
-    version = "0.5.5",
+    version = "0.5.6",
     description = "Library for CrystalFontz and Matrix-Orbital LCD displays",
     author = "mike wakerly",
     author_email = "opensource@hoho.com",
